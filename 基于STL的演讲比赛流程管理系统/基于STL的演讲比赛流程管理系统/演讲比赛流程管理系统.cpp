51a00d6dd3b603550696e594624d8beac0a34361
wlx812 <60246200+wlx812@users.noreply.github.com>
2020-02-10 17:43:16 +0800

    STL_Speech_Test
    C++about STL

---
+++ 基于STL的演讲比赛流程管理系统/基于STL的演讲比赛流程管理系统/演讲比赛流程管理系统.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "speechManager.h"
+#include <string>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	// 随机数种子
+	srand((unsigned int)time(NULL));
+	// 创建管理类的对象
+	SpeechManager sm;
+	/*注释：   先CTRL + K，然后CTRL + C    取消注释： 先CTRL + K，然后CTRL + U*/
+	/*for (map<int, Speaker>::iterator it = sm.m_Speaker.begin(); it != sm.m_Speaker.end(); it++)
+	{
+		cout << "选手编号：" << it->first << "姓名：" << it->second.m_Name << "分数：" << it->second.m_Score[0] << endl;
+	}*/
+	int choice = 0; // 用于存储用户输入
+	while (true)
+	{
+		sm.show_Menu();
+		cout << "请输入您的选择" << endl;
+		cin >> choice;
+		switch (choice)
+		{
+		case 1: // 开始比赛
+			sm.startSpeech();
+			break;
+		case 2: // 查看往届比赛记录
+			sm.showRecord();
+			break;
+		case 3: // 清空比赛记录
+			sm.clearRecord();
+			break;
+		case 0: // 退出系统
+			sm.exitSystem();
+			break;
+		default:
+			system("cls");
+			break;
+		}
+	}
+	
+	system("pause");
+	return 0;
+}
